7f428221989fec664712674064d951ecca82fe53
Kosta <68782786+KostaD02@users.noreply.github.com>
2025-08-10 00:41:57 +0400

    chore: init base app

---
+++ libs/home/src/lib/home/home.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-home',
+  templateUrl: './home.html',
+  styleUrl: './home.scss',
+})
+export class Home {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('@angular-firebase-hosting-example/home').then((m) => m.Home),
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    loadComponent: () =>
+      import('@angular-firebase-hosting-example/not-found').then(
+        (m) => m.NotFound
+      ),
+  },
+];
